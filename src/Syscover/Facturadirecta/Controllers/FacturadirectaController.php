7e32d38d13e58c5edda942a25a6f7d457a56899b
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-04-20 13:39:48 +0200

    refactor models and controllers

--- src/Syscover/Facturadirecta/Controllers/FacturadirectaController.php
+++ src/Syscover/Facturadirecta/Controllers/FacturadirectaController.php
@@ -1,15 +1,16 @@
 <?php namespace Syscover\Facturadirecta\Controllers;
 
+// todo, hace falta que herede de este controlador?
+use Syscover\Pulsar\Core\Controller;
 use Syscover\Facturadirecta\Facades\Facturadirecta;
-use Syscover\Pulsar\Controllers\Controller;
 
 /**
  * Class FacturadirectaController
  * @package Syscover\Facturadirecta\Controllers
  */
 
-class FacturadirectaController extends Controller {
-
+class FacturadirectaController extends Controller
+{
     public function getClients()
     {
         $fdCustomers    = Facturadirecta::getClients($this->request->all());
